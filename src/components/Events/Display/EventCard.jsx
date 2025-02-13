9f2f09dfe674ac72bf073a023a6f60bbfd001ec1
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-02-13 14:15:16 -0600

    categories for current and future events

--- src/components/Events/Display/EventCard.jsx
+++ src/components/Events/Display/EventCard.jsx
@@ -15,31 +15,40 @@ const EventCard = ({ organisationName, eventName, description, eventDate, startT
     const title = capitaliseText.join(" ").trim();
 
     return (
-        <div className="event-card">
-            <div className="price">
-                {price === 0 ? "Free" : `$${price}`}
+        <div>
+            <div>
+            {new Date().toLocaleString("default", {
+                weekday: "short",
+                day: "numeric",
+                month: "short",
+            })}
             </div>
-            <div className="event-header">
-                <h3 className="event-title">{title}</h3>
-            </div>
-            <div className="event-description">
-                <div onClick={()=> setOpenModal(true)}>
-                {openmodal ? ( <><div onClick={() => setOpenModal(false)}><p>See Description</p> <AiOutlineUp /></div></>) : (<><div onClick={() => setOpenModal(true)}><p>See Description</p> <AiOutlineDown /></div></>)}
+            <div className="event-card">
+                <div className="price">
+                    {price === 0 ? "Free" : `$${price}`}
                 </div>
-                {openmodal && <p>{description}</p>}
-            </div>           
-            <div className="event-location">
-                <p>{addressLine1}</p>
-                <p>{addressLine2}</p>
-                <p>{city}, {postcode}</p>
-            </div>
-            <div className="event-dates-time">
-                <p>{organisationName}</p>
-                <p>{date}</p>
-                <p>{startTime} - {endTime}</p>
-            </div>
-            <div className="event-url">
-                <a href={eventURL} target="_blank" rel="noopener noreferrer">View Event</a>
+                <div className="event-header">
+                    <h3 className="event-title">{title}</h3>
+                </div>
+                <div className="event-description">
+                    <div onClick={()=> setOpenModal(true)}>
+                    {openmodal ? ( <><div onClick={() => setOpenModal(false)}><p>See Description</p> <AiOutlineUp /></div></>) : (<><div onClick={() => setOpenModal(true)}><p>See Description</p> <AiOutlineDown /></div></>)}
+                    </div>
+                    {openmodal && <p>{description}</p>}
+                </div>           
+                <div className="event-location">
+                    <p>{addressLine1}</p>
+                    <p>{addressLine2}</p>
+                    <p>{city}, {postcode}</p>
+                </div>
+                <div className="event-dates-time">
+                    <p>{organisationName}</p>
+                    <p>{date}</p>
+                    <p>{startTime} - {endTime}</p>
+                </div>
+                <div className="event-url">
+                    <a href={eventURL} target="_blank" rel="noopener noreferrer">View Event</a>
+                </div>
             </div>
         </div>
     )
--- src/components/Events/Display/EventSchedule.jsx
+++ src/components/Events/Display/EventSchedule.jsx
@@ -1,33 +1,98 @@
-import React from 'react'
-import EventCard from './EventCard';
+import { React } from "react";
+import { BsCaretDownFill } from "react-icons/bs";
+import EventCard from "./EventCard";
 
-const EventSchedule = ({events}) => {
+const EventSchedule = ({ events }) => {
+  const eventsThisMonth = [];
+  const futureEvents = [];
+
+  events.map((item) => {
+    const currentMonth = new Date().getMonth();
+    const currentYear = new Date().getFullYear();
+    const itemMonth = new Date(item.eventDate).getMonth();
+    const itemYear = new Date(item.eventDate).getFullYear();
+    if (itemMonth + itemYear === currentMonth + currentYear) {
+      eventsThisMonth.push(item);
+    }
+    if ( itemYear >= currentYear && itemMonth > currentMonth){
+      futureEvents.push(item);
+    }
+  });
+
   return (
     <div>
       <div>
-        <h3>{new Date().toLocaleString("default",{ month: "long", year: "numeric" })}</h3>
+        <h3>
+          {new Date().toLocaleString("default", {
+            month: "long",
+            year: "numeric",
+          })}
+        </h3>
         {/* to add dropdown here */}
         <h4>Month</h4>
+        <BsCaretDownFill />
       </div>
-        {events.map(item => 
-            <EventCard 
-                key = {item._id}
-                organisationName = {item.organisationName}
-                eventName = {item.eventName}
-                description={item.description}
-                eventDate = {item.eventDate}
-                startTime = {item.startTime}
-                endTime = {item.endTime}
-                addressLine1 = {item.location.addressLine1}
-                addressLine2 = {item.location.addressLine2}
-                city = {item.location.city}
-                postcode = {item.location.postcode}
-                eventURL = {item.eventURL}
-                price = {item.price}
-            />
-        )}
+      <div>
+        {new Date().toLocaleString("default", {
+          weekday: "short",
+          day: "numeric",
+          month: "short",
+        })}
+      </div>
+      <div>
+      {eventsThisMonth.length !== 0 ? (
+        <div>
+        {eventsThisMonth.map((item) => (
+        <EventCard
+          key={item._id}
+          organisationName={item.organisationName}
+          eventName={item.eventName}
+          description={item.description}
+          eventDate={item.eventDate}
+          startTime={item.startTime}
+          endTime={item.endTime}
+          addressLine1={item.location.addressLine1}
+          addressLine2={item.location.addressLine2}
+          city={item.location.city}
+          postcode={item.location.postcode}
+          eventURL={item.eventURL}
+          price={item.price}
+        />
+        ))}
+        </div>
+      ) : (
+        <div>
+          <p>There are no events this month!</p>
+        </div>
+      )}
+      </div>
+      <div>
+        <h2>Upcoming events:</h2>
+        {futureEvents.length !== 0 && 
+          <div>
+        {futureEvents.map((item) => (
+        <EventCard
+          key={item._id}
+          organisationName={item.organisationName}
+          eventName={item.eventName}
+          description={item.description}
+          eventDate={item.eventDate}
+          startTime={item.startTime}
+          endTime={item.endTime}
+          addressLine1={item.location.addressLine1}
+          addressLine2={item.location.addressLine2}
+          city={item.location.city}
+          postcode={item.location.postcode}
+          eventURL={item.eventURL}
+          price={item.price}
+        />
+      ))}
+        </div>
+        }
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
 export default EventSchedule;
